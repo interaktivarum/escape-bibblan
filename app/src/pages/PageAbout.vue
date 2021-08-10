8c7bc496f1d0b35e3b4fdcbd8204ea6b604df98a
Martin Törnros <martin@interaktivarum.se>
2021-08-10 14:28:31 +0200

    Github link

--- app/src/pages/PageAbout.vue
+++ app/src/pages/PageAbout.vue
@@ -4,22 +4,18 @@
     <div class="row flex-center">
       <div class="col-10">
 
-        <h4>Om Escape Bibblan</h4>
-
+        <h2>Om Escape Bibblan</h2>
         <p>
           Escape Bibblan är en digital version av populära Escape rooms, i vilka deltagarna i en fysisk miljö samarbetar för att lösa gåtor och problem samt för att ta sig ut ur låsta rum.
         </p>
-
         <p>
           I Escape Bibblan presenteras spelet och utmaningen på denna webbplats och samarbetet sker digitalt över valfri videoplattform på datorn eller telefonen.
         </p>
 
-        <h5>Digital kompetens </h5>
-
+        <h3>Digital kompetens </h3>
         <p>
           Escape Bibblan är en spelprototyp designad och skapad av <a href="https://interaktivarum.se" target="_blank">Interaktiva rum</a> i samverkan med <a href="https://regionsormland.se/" target="_blank">Region Sörmland</a> och <a href="https://www.regionostergotland.se/" target="_blank">Region Östergötland</a>.
         </p>
-
         <p>
           Syftet är att öka den digitala kompetensen, särskilt hos äldre medborgare, genom att samarbeta och ha roligt tillsammans över nätet! Att lära genom att göra, helt enkelt.
         </p>
@@ -28,18 +24,21 @@
            Utmaningarna designas för att kräva olika typer av kompetenser, alltifrån historiekunnande och att se logiska samband till att använda sökmotorer för att hitta ny kunskap eller att tänka utanför boxen. En bredd av kompetenser inom gruppen är en styrka.
         </p>
 
-        <h5>Inspiration</h5>
-
+        <h3>Inspiration</h3>
         <p>
           Det finns flera goda exempel på digitala escape rooms och liknande upplevelser som används i såväl klassrummet och biblioteksmiljö. <a href="https://digidel.se/teknikjakten/index.html" target="_blank">Teknikjakten</a> av Umeå kommuns bibliotek bjuder in användaren till att använda digitala tjänster för att lösa olika uppgifter, i en rolig och klurig lärupplevelse. Googles digitala verktyg (<a href="https://www.google.com/slides/about" target="_blank">Slides</a>+<a href="https://www.google.com/forms/about" target="_blank">Forms</a>) är populära hos lärare för att skapa presentationer med klickbara ytor och villkorsstyrda inmatningsfält för att ett lustfyllt och spelinspirerat lärande.
         </p>
-
         <p>
           Escape Bibblan tar inspiration från ovan nämnda exempel och ska särskilt ses som en teknisk vidareutveckling av dessa, med nya spännande möjligheter till interaktion och samarbete.
           I nämnda exempel kan uppgifterna lösas på egen hand eller i grupp, och samtliga deltagare presenteras i dessa verktyg att för exakt samma information. Varje deltagare behöver själv fylla i rätt svar och klicka sig vidare, då webbplatserna saknar uppfattning om att fler personer är inne samtidigt och samarbetar.
         </p>
         <p>
           En betydande funktion i Escape Bibblan är möjligheten att presentera <i>olika</i> ledtrådar och utmaningar till varje deltagare och där samtliga spelare förflyttas automatiskt och tillsammans mellan de olika rummen och utmaningarna. Detta bidrar till en mer dynamisk upplevelse och starkare tillsammanskänsla som förutsätter ett fördjupat samarbete och en större bredd av kompetenser.
+        </p>
+
+        <h3>Öppen licens</h3>
+        <p>
+          Escape Bibblan är utvecklad med JavaScript-ramverket <a href="https://quasar.dev/" target="_blank">Quasar</a> och använder WebSockets för kommunikation mellan användare. All källkod är släppt under öppen licens (<a href="https://opensource.org/licenses/MIT" target="_blank">MIT</a>) och finns <a href="https://github.com/interaktivarum/escape-bibblan" target="_blank">tillgänglig på Github</a> för vem som helst att bygga vidare på.
         </p>
 
       </div>
